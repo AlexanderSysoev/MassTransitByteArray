8cd932fc895081ff265ac3bb2bc44dbdb82f69c4
AlexanderSysoev <asysoev@list.ru>
2018-08-31 14:41:50 +0300

    Initial commit

---
+++ MassTransitByteArray/TestClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+using NUnit.Framework;
+
+namespace MassTransitByteArray
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public async Task Test()
+        {
+            var busControl = Bus.Factory.CreateUsingInMemory(config =>
+            {
+            });
+
+            busControl.Start();
+
+            var sendEndpoint = await busControl.GetSendEndpoint(new Uri("http://test.ru"));
+
+            Assert.DoesNotThrowAsync(async () =>
+            {
+                await sendEndpoint.Send<SomeContract>(new
+                {
+                    Bytes = new byte[] { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 }
+                });
+            });
+        }
+    }
+
+    public interface SomeContract
+    {
+        byte[] Bytes { get; set; }
+    }
+}
